682084b2a571e35eeafd45066d279440f31e426e
pppsihhh <ust.boyko@gmail.com>
2021-10-25 23:41:41 +0300

    add h9-l1-1

---
+++ H9-L1-1-MethodList/src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		printList();
+
+	}
+
+	public static void printList() {
+		List<Integer> a = new ArrayList<>(10);
+		for (int i = 0; i < 10; i++) {
+			a.add((int)(Math.random() * 10 + 1));
+		}
+		
+		a.remove(0);
+		a.remove(1);
+		a.remove(a.size()-1);
+		
+		System.out.println(a);
+	}
+
+}
